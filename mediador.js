f720b652cbfb54e6893a7a60c441aa8737550014
LupitaOliver22 <157176353+LupitaOliver22@users.noreply.github.com>
2025-09-03 23:22:50 -0300

    Update mediador.js

--- mediador.js
+++ mediador.js
@@ -1,20 +1,6 @@
-// ==UserScript==
-// @name         Mediador Loader Oliverex
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Carrega e executa Oliverex.js diretamente do seu GitHub automaticamente
-// @author       Você
-// @match        https://educacao.sp.gov.br/*   // ajuste conforme necessário
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-    
-    const script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/LupitaOliver22/OliverExpans-o/main/Oliverex.js';
-    script.type = 'text/javascript';
-    script.onload = () => console.log('Oliverex.js carregado com sucesso!');
-    script.onerror = () => console.error('Falha ao carregar Oliverex.js');
-    document.head.appendChild(script);
+javascript:(function(){
+    const s=document.createElement('script');
+    s.src='https://raw.githubusercontent.com/LupitaOliver22/OliverExpans-o/main/Oliverex.js';
+    s.type='text/javascript';
+    document.head.appendChild(s);
 })();
