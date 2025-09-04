7e1369f35fb63c225959cae9b9e3cf9da4d41c65
LupitaOliver22 <157176353+LupitaOliver22@users.noreply.github.com>
2025-09-03 23:25:25 -0300

    Update mediador.js

--- mediador.js
+++ mediador.js
@@ -2,5 +2,7 @@ javascript:(function(){
     const s=document.createElement('script');
     s.src='https://raw.githubusercontent.com/LupitaOliver22/OliverExpans-o/main/Oliverex.js';
     s.type='text/javascript';
+    s.onload = () => alert("✅ Oliverex.js carregado com sucesso!");
+    s.onerror = () => alert("❌ Erro ao carregar Oliverex.js");
     document.head.appendChild(s);
 })();
